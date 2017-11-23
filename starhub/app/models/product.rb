fc0cae3e9b55e94b92e9f890a2855bfa53cb02f2
Ignacio Jonas <ignacio.jonas@ar.ibm.com>
2017-11-23 17:41:39 -0300

    Relationshipts - Chapter 4

---
+++ starhub/app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  has_many :reviews
+  has_many :issues
+  has_many :reviewing_customers, through: :reviews, source: :customer
+  has_many :complaining_customers, through: :issues, source: :customer
+
+end
